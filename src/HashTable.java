2b0c4ca9d6109fa0ce375e8af1689b256cbe3e67
AJ <anthonyj0127@gmail.com>
2026-01-27 21:01:01 -0800

    Created HashTable class and hashing method

---
+++ src/HashTable.java
@@ -0,0 +1,53 @@
+public class HashTable {
+
+    private final int size = 7;
+    private Node[] dataMap;
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public class Node {
+
+        String key;
+        int value;
+        Node next;
+
+        public Node(String key, int value){
+            
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+
+    public void printTable(){
+
+        for (int i = 0; i < dataMap.length; i++){
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null){
+                System.out.println("   {" + temp.key + "=" + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+
+    }
+
+    private int hash(String key){
+
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+
+        for (int i = 0; i < keyChars.length; i++){
+
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+
+        }
+
+        return hash;
+
+    }
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,91 +1,11 @@
-import java.util.HashMap;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-//
-//        LinkedList myLinkedList = new LinkedList(4);
-//
-//        myLinkedList.getHead();
-//        myLinkedList.getTail();
-//        myLinkedList.getLength();
-//
-//        //Test append method
-//        myLinkedList.append(2);
-//        myLinkedList.printList();
-//
-//        //Test remove last method
-//        myLinkedList.removeLast();
-//        myLinkedList.getTail();
-//        myLinkedList.removeLast();
-//
-//        //Test prepend method
-//        myLinkedList.prepend(2);
-//        myLinkedList.prepend(5);
-//        myLinkedList.printList();
-//
-//        //Test remove first node method
-//        myLinkedList.removeFirst();
-//        myLinkedList.printList();
-//
-//        //Test get method
-//        myLinkedList.append(35);
-//        myLinkedList.append(321);
-//        myLinkedList.append(24);
-//        System.out.println(myLinkedList.get(2).value);
-//
-//        //Test set method
-//        myLinkedList.set(2, 34);
-//        System.out.println(myLinkedList.get(2).value);
-//
-//        // Test insert method
-//        System.out.println("Current Linked List: ");
-//        myLinkedList.printList();
-//
-//        myLinkedList.insert(2, 204);
-//        System.out.println("My linked list after inserting 204 at index 2: ");
-//        myLinkedList.printList();
-//
-//        //Test remove method
-//        System.out.println("List before removal");
-//        myLinkedList.printList();
-//
-//        System.out.println("Removing node at index 2");
-//        myLinkedList.remove(2);
-//        myLinkedList.printList();
-//
-//        // Test reverse method
-//        System.out.println("List before reversing");
-//        myLinkedList.printList();
-//
-//        System.out.println("List after reversing");
-//        myLinkedList.reverse();
-//        myLinkedList.printList();
 
-        // ############### Doubly Linked List Test ###############
+        HashTable myTable = new HashTable();
 
-        DoublyLinkedList myDll = new DoublyLinkedList(8);
-        myDll.append(8);
-        myDll.append(234);
-        myDll.append(593);
-        myDll.append(1);
-        myDll.append(78);
-        myDll.printList();
-
-        System.out.println(myDll.get(4).value);
-
-        myDll.insert(38, 4);
-        System.out.println(myDll.get(4).value);
-
-        System.out.println(myDll.remove(4).value);
-        System.out.println(myDll.get(4).value);
-
-        Queue myQueue = new Queue(8);
-        myQueue.getLast();
-        myQueue.enqueue(38);
-        myQueue.getLast();
+        myTable.printTable();
 
     }
 
